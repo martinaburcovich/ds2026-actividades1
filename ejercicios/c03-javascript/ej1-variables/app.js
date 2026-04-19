171fc71a724e8dcfa635319c77a7e5dcd572fd69
Martina Ana Burcovich <burcovichmartinaa@gmail.com>
2026-04-19 19:18:22 -0300

    feat: ejercicio 1 variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,12 @@
+const nombre = "Martina Ana Burcovich";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log(contador);
